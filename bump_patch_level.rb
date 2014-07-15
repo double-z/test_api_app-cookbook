478beaa1789dab4baaa03997e593e9f9dc857894
double-z <zackzondlo@gmail.com>
2014-07-14 21:46:10 -0400

    bump

--- bump_patch_level.rb
+++ bump_patch_level.rb
@@ -49,4 +49,13 @@ if updated
   if !system("git push origin master")
     raise "Failed to git push"
   end
+
+  require 'octokit'
+  client = Octokit::Client.new :access_token => '23d05f65bd694c2ab64f23d5ca581d6116dd6569'
+  if File.exists?('VERSION')
+    new_version=`cat VERSION`
+  end
+  puts "Creating Release #{new_version}"
+  client.create_release("double-z/test_api_app-cookbook", new_version.to_s.chomp, {"target_commitish" => "master", "name" => "#{new_version.to_s.chomp}"})
+
 end
--- test.rb
+++ test.rb
@@ -1,22 +1,7 @@
 require 'octokit'
-user = Octokit.user 'double-z'
 client = Octokit::Client.new :access_token => '23d05f65bd694c2ab64f23d5ca581d6116dd6569'
 if File.exists?('VERSION')
   new_version=`cat VERSION`
 end
-#  :login    => 'double-z',
-#  :password => 'Florida100'
-# Get the repos rel, returned from the API
-# as repos_url in the resource
-#user.rels[:releases].href
-#user.rels[:repos].href
-# => "https://api.github.com/users/technoweenie/repos"
-#repo = Octokit.repo 'double'
-#rel = repo.rels[:issues]
 puts "Creating Release #{new_version}"
 client.create_release("double-z/test_api_app-cookbook", new_version.to_s.chomp, {"target_commitish" => "master", "name" => "#{new_version.to_s.chomp}"})
-#puts b.inspect
-#repos = user.rels[:repos].get.data
-#puts repos.last.name
-# => "faraday-zeromq"
-
