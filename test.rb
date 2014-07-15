d1c1aed322ee44ee68cec3772f33dac9e74cce60
double-z <zackzondlo@gmail.com>
2014-07-14 21:28:35 -0400

    blah

--- test.rb
+++ test.rb
@@ -1,7 +1,9 @@
 require 'octokit'
 user = Octokit.user 'double-z'
 client = Octokit::Client.new :access_token => '23d05f65bd694c2ab64f23d5ca581d6116dd6569'
-
+if File.exists?('VERSION')
+  new_version=`cat VERSION`
+end
 #  :login    => 'double-z',
 #  :password => 'Florida100'
 # Get the repos rel, returned from the API
@@ -11,7 +13,8 @@ client = Octokit::Client.new :access_token => '23d05f65bd694c2ab64f23d5ca581d611
 # => "https://api.github.com/users/technoweenie/repos"
 #repo = Octokit.repo 'double'
 #rel = repo.rels[:issues]
-client.create_release("double-z/test_api_app-cookbook", "0.0.17", {"target_commitish" => "master", "name" => "0.0.17"})
+puts "Creating Release #{new_version}"
+client.create_release("double-z/test_api_app-cookbook", new_version.to_s.chomp) #, {"target_commitish" => "master", "name" => "#{new_version.to_s}"})
 #puts b.inspect
 #repos = user.rels[:repos].get.data
 #puts repos.last.name
